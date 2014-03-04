c087fd1e7a632b1d5d1eb6e1340f35dba42b8984
neilkenney <neilkenney@10.30.16.16>
2014-03-03 20:58:11 -0600

    Fixed CashRegisters dependency on ReceiptConsoleOutputStrategy

--- src/pointofsale/PointOfSale.java
+++ src/pointofsale/PointOfSale.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package pointofsale;
 
@@ -16,7 +12,7 @@ public class PointOfSale {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CashRegister cr = new CashRegister();
+        CashRegister cr = new CashRegister(new ReceiptJOptionPaneOutput());
         cr.startNewSale(new FakeDatabase(), "100");
         cr.addNewItem(new FakeDatabase(), "P123", 1.0);
         cr.addNewItem(new FakeDatabase(), "A123", 2.0);
