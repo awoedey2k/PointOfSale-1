053c8b05f04c092c9ce485bcb33a839a05b06e9e
neilkenney <neilkenney@192.168.0.105>
2014-03-26 19:31:04 -0500

    redid try/catch in point of sale class

--- src/pointofsale/PointOfSale.java
+++ src/pointofsale/PointOfSale.java
@@ -13,14 +13,12 @@ public class PointOfSale {
      */
     public static void main(String[] args) {
         CashRegister cr = new CashRegister(new ReceiptJOptionPaneOutput());
-        try{
+        
         cr.startNewSale(new FakeDatabase(), "1000", new TaxPercent(.05));
         cr.addNewItem(new FakeDatabase(), "P123", 1.0);
         cr.addNewItem(new FakeDatabase(), "A123", 2.0);
         cr.finalizeSale();
-        }catch(IllegalArgumentException ex){
-            cr.printExceptions(ex);
-        }
+        
 }
     
 }
